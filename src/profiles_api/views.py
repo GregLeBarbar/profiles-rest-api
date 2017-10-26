afc073a4c38aa642d569d10ab5bb0491b5f67d98
GregLeBarbar <gregory.charmier.epfl.ch>
2017-10-26 08:50:40 +0200

    API Profile first version

--- src/profiles_api/views.py
+++ src/profiles_api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserProfileSerializer
+from .models import UserProfile
+
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+    serializer_class = UserProfileSerializer
+    queryset = UserProfile.objects.all()
